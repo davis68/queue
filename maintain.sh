1c25dfccfd76d09c324e5ea6953538bdef2a553a
davis68 <davis68@illinois.edu>
2016-06-21 13:27:13 -0500

    Adding nohup to script.

--- maintain.sh
+++ maintain.sh
@@ -5,12 +5,12 @@ export QUEUE_HOME=$HOME/public_html/queue
 case "ps x | grep -v grep | grep -c 'queue.py'" in
 
 0)  echo "Restarting Queue:     $(date)" >> $HOME/queue.log
-    python $QUEUE_HOME/queue.py
+    nohup python $QUEUE_HOME/queue.py &
     ;;
 1)  # all ok
     ;;
-*)  echo "Removed double Queue: $(date)" >> $HOME/queue.log
-    kill $(pidof queue.py | awk '{print $1}')
-    ;;
+#*)  echo "Removed double Queue: $(date)" >> $HOME/queue.log
+#    kill $(pidof python | awk '{print $1}')
+#    ;;
 esac
 at -f ./maintain.sh now + 1 minute
