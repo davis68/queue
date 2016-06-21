08b6b928b3855aea723e8aa6f28f3e01b9d779b3
davis68 <davis68@illinois.edu>
2016-06-21 13:55:54 -0500

    Fixing more process counting issues with the restart script.

--- maintain.sh
+++ maintain.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 export QUEUE_HOME=$HOME/public_html/queue
-
-case "ps x | grep -v grep | grep -c 'queue.py'" in
+proc_count=$(ps x | grep -v grep | grep -c "queue.py")
+case $proc_count in
 
 0)  echo "Restarting Queue:     $(date)" >> $HOME/queue.log
     nohup python $QUEUE_HOME/queue.py &
     ;;
 *)  # all ok
+    #echo $proc_count
+    #echo $(ps x | grep -v grep | grep -c 'queue.py')
     ;;
 #*)  echo "Removed double Queue: $(date)" >> $HOME/queue.log
 #    kill $(pidof python | awk '{print $1}')
